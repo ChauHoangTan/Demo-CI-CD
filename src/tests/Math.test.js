2aa3505f6b4d1f6ed605fafcd91b9931583331e5
ChauHoangTan <chauhoangtan6937@gmail.com>
2023-12-11 14:36:45 +0700

    add Test case

--- src/tests/Math.test.js
+++ src/tests/Math.test.js
@@ -8,3 +8,7 @@ test('adds 1 + 2 to equal 3', () => {
 test('adds -1 + 1 to equal 0', () => {
   expect(add(-1, 1)).toBe(0);
 });
+
+test('adds -3 + -1 to equal -4', () => {
+    expect(add(-3, -1)).toBe(-4);
+  });
